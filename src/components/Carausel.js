b3c89f969a047c784086b37ac522d1b12b79ddd9
SelimSevgi <slmsvg44@gmail.com>
2022-03-31 23:55:30 +0300

    carausel-2

--- src/components/Carausel.js
+++ src/components/Carausel.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdOutlineIosShare } from "react-icons/md";
+import { BsArrowRight } from "react-icons/bs";
 export default function Carausel() {
   return (
     <div className="w-full h-[540px] bg-orange-400 flex flex-col items-center">
@@ -26,12 +27,19 @@ export default function Carausel() {
       <div className=" w-[1088px] flex flex-row items-center justify-between bg-white rounded-[10px] mt-4">
         <div className="w-[408px] h-[386px]  flex items-center justify-center relative">
           <MdOutlineIosShare className="w-[40px] h-[40px] p-2 absolute top-0 right-0 text-gray-500 mr-[10px] mt-[30px] font-light transition ease-in-out delay-2000 hover:bg-slate-300 hover:rounded-full  " />
-          <div className="flex ml-[10px] flex-col items-center justify-center ">
-            <span className="font-semibold text-[32px] tracking-[.34px] ">
+          <div className="flex ml-[10px] flex-col   ">
+            <span className="font-semibold text-[32px] tracking-[.34px] leading-[1.25] ">
               Hediyeler, fırsatlar
             </span>
-            <span>ve çok daha fazlasını keşfet</span>
-            <a href="https://sanaozel.hepsiburada.com/">Hemen incele</a>
+            <span className="mt-2 text-[24px]">
+              ve çok daha fazlasını keşfet
+            </span>
+            <a
+              className="flex  items-center justify-center w-[135px] h-[40px] mt-10 text-white bg-orange-500 font-bold text-[14px] mr-8 cursor-pointer transition ease-in-out delay-100 p-2 rounded-3xl "
+              href="https://sanaozel.hepsiburada.com/"
+            >
+              Hemen incele
+            </a>
           </div>
         </div>
         <div className="flex items-center justify-center">
@@ -42,7 +50,16 @@ export default function Carausel() {
           ></img>
         </div>
       </div>
-      <div>altdasda</div>
+      <div>
+        <img
+          className="w-[71px] h-[40px]"
+          src="https://images.hepsiburada.net/banners/s/0/71-40/thumbnailImage2112_20220314142732.png/format:webp"
+        ></img>
+        <div className="w-[200px] h-[36px] flex flex-row border-[1px] rounded-2xl">
+          Bugüne Özel
+          <BsArrowRight />
+        </div>
+      </div>
     </div>
   );
 }
